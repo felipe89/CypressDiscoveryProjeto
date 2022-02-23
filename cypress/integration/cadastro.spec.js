4c21def5f7e1e5723a64bff77febdbd9ec5d619a
Felipe <felipe@pop-os.localdomain>
2022-02-23 18:47:27 -0300

    Massa de testes em arq Fixtures

--- cypress/integration/cadastro.spec.js
+++ cypress/integration/cadastro.spec.js
@@ -1,84 +1,25 @@
-import SignupPage from '../pages/SignupPage'
+import signup from "../pages/SignupPage";
 
-describe('Cadstro', ()=>{
+describe("Cadstro", () => {
+  beforeEach(function () {
+    cy.fixture("deliver").then((massaTeste) => {
+      this.deliver = massaTeste;
+    });
+  });
 
-    //Ganchos do cypress
-    before(function() {
-        cy.log('Tudo aqui é executado uma unica vez ANTES de TODOS os casos de testes')
-    })
+  it("Usuario deve se cadastrar para se tornar um entregador", function () {
+    signup.acessarPaginaBuerEats();
+    signup.preencherFormulario(this.deliver.signup);
+    signup.submeterFormulario();
+    const expectedMessage =
+      "Recebemos os seus dados. Fique de olho na sua caixa de email, pois e em breve retornamos o contato.";
+    signup.modalValidaCadastro(expectedMessage);
+  });
 
-    beforeEach(function() {
-        cy.log('Tudo aqui é executado sempre ANTES de CADA caso de teste')
-    })
-
-    after(function() {
-        cy.log('Tudo aqui é executado uma unica vez DEPOSI de TODOS os casos de testes')
-    })
-
-    afterEach(function() {
-        cy.log('Tudo aqui é executado sempre DEPOIS de CADA caso de teste')
-    })
-
-
-    it ('Usuario deve se cadastrar para se tornar um entregador', ()=>{
-        
-        //Gerando massa de testes para preenchimento de formulario por variavel
-        var entregador = {
-            nome: 'Felipe',
-            cpf: '00011133311',
-            email: 'teste@automacao.com.br',
-            whatsapp: '11999999999',
-            endereco: {
-                cep:'08330415',
-                rua:'Rua Galáxia',
-                numero:'955',
-                complento:'Em frente a praça',
-                bairro:'Cidade Satélite Santa Bárbara',
-                cidade_uf:'São Paulo/SP'
-            },
-            metodo_entrega:'Moto',
-            metodo_entrega_2:'Bicicleta',
-            metodo_entrega_3: 'Van/Carro',
-            cnh:'cnh_gratuita.jpg'
-        }
-
-        var signup = new SignupPage()
-
-        signup.acessarPaginaBuerEats()
-        signup.preencherFormulario(entregador)
-        signup.submeterFormulario()
-        const expectedMessage = 'Recebemos os seus dados. Fique de olho na sua caixa de email, pois e em breve retornamos o contato.'
-        signup.modalValidaCadastro(expectedMessage)
-    })
-
-it ('CPF Invalido', ()=>{
-   
-    //Gerando massa de testes para preenchimento de formulario por variavel
-    var entregador = {
-        nome: 'Felipe',
-        cpf: '000111333BF',
-        email: 'teste@automacao.com.br',
-        whatsapp: '11999999999',
-        endereco: {
-            cep:'08330415',
-            rua:'Rua Galáxia',
-            numero:'955',
-            complento:'Em frente a praça',
-            bairro:'Cidade Satélite Santa Bárbara',
-            cidade_uf:'São Paulo/SP'
-        },
-        metodo_entrega:'Moto',
-        metodo_entrega_2:'Bicicleta',
-        metodo_entrega_3: 'Van/Carro',
-        cnh:'cnh_gratuita.jpg'
-    }
-
-    var signup = new SignupPage()
-
-    signup.acessarPaginaBuerEats()
-    signup.preencherFormulario(entregador)
-    signup.submeterFormulario()
-    signup.menssagemAlerta('Oops! CPF inválido')
-
-    })
-})
+  it("CPF Invalido", function () {
+    signup.acessarPaginaBuerEats();
+    signup.preencherFormulario(this.deliver.cpf_invalido);
+    signup.submeterFormulario();
+    signup.menssagemAlerta("Oops! CPF inválido");
+  });
+});
--- cypress/pages/SignupPage.js
+++ cypress/pages/SignupPage.js
@@ -48,4 +48,4 @@ class SignupPage{
     }
 }
 
-export default SignupPage; 
+export default new SignupPage; 
